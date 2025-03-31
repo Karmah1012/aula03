41e4de02d247bcbb9b7df26764fcb87bc656516b
Karmah <karmah1012@gmail.com>
2025-03-31 14:57:46 -0300

    adicionado as 4 operações numericas

---
+++ exercicio03.py
@@ -0,0 +1,13 @@
+numero = float(input("Qual é o primeiro número?"))
+numero2 = float(input("Qual é o segundo número?"))
+soma = numero + numero2
+print (f"O somatório entre os dois números é: {soma}")
+subtraciones = numero - numero2
+print (f"A subtração entre esses dois números é: {subtraciones}")
+multih = numero*numero2
+print (f"A multiplicação entre esses dois números, é: {multih} ")
+divih = numero/numero2
+print (f"A divisão entre esses dois números, é: {divih}")
+
+
+
