b4e68e57d7ddf34dfdd6f18431b03d7f4766a0f2
Karmah <karmah1012@gmail.com>
2025-03-31 16:24:06 -0300

    media dos alunos adicionada

---
+++ exercicio04.py
@@ -0,0 +1,6 @@
+n1 = int(input("Quanto vale n1?"))
+n2 = int(input("Quanto vale n2?"))
+if n1 < n2 :
+    print(n1,n2)
+else:
+    print(n2,n1)
---
+++ exercicio05.py
@@ -0,0 +1,9 @@
+n1 = float(input("Qual a primeira nota?"))
+n2 = float(input("Qual a segunda nota?"))
+n3 = float(input("Qual a terceira nota?"))
+media = float(n1+n2+n3)/3
+print(f"A média do aluno, é: {media}")
+if media < 7 :
+    print("Aluno REPROVADO!")
+if media >= 7 :
+    print("Aluno APROVADO!")
