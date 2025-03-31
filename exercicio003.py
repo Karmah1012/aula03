af53f57aa08f91b7249c2a4724468ca9baa54713
Karmah <karmah1012@gmail.com>
2025-03-31 15:57:11 -0300

    adicionado valor das variaveis a b c

---
+++ exercicio003.py
@@ -0,0 +1,9 @@
+a = int(input("Qual é o valor de A?"))
+print (f"A tem o valor de: {a} ")
+b = int(input("Qual é o valor de b?"))
+print (f"B tem o valor de: {b}")
+c = a
+a = b
+b = c
+print(f"Valor novo de A: {a}, valor novo de B: {b}")
+
