056a13b53a070f8652e3be660b2bc56a95fe6025
Karmah <karmah1012@gmail.com>
2025-03-31 16:52:41 -0300

    adicionado a categoria recuperação

--- exercicio05.py
+++ exercicio05.py
@@ -3,7 +3,10 @@ n2 = float(input("Qual a segunda nota?"))
 n3 = float(input("Qual a terceira nota?"))
 media = float(n1+n2+n3)/3
 print(f"A média do aluno, é: {media}")
-if media < 7 :
-    print("Aluno REPROVADO!")
 if media >= 7 :
     print("Aluno APROVADO!")
+else:
+    if media < 4:
+        print(f"Aluno reprovado {media}")
+    else:
+        print(f"Aluno em recuperação {media}")
